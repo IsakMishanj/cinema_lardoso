a24d6bd7daecca90155d2d2d39a0a15e45ec3948
eboiardi <emilio.boiardi.studenti@isii.it>
2024-12-11 11:55:37 +0100

    lardo nuovo

--- js/pages.js
+++ js/pages.js
@@ -2,3 +2,11 @@ function openReviewPage(movieId) {
     // Redirige alla pagina del film
     window.location.href = `movie.html?id=${movieId}`;
 }
+
+
+
+document.getElementById("search-button").addEventListener("click", function() {
+    const query = document.getElementById("search-input").value.toLowerCase();
+    alert(`Searching for: ${query}`);
+    // Implementa la logica di ricerca qui
+});
