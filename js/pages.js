6403d9c726c07a37a6f59d0ae3ba114a8fdb2e40
eboiardi <emilio.boiardi.studenti@isii.it>
2024-12-12 11:48:30 +0100

    movie-details 1.0

--- js/pages.js
+++ js/pages.js
@@ -2,11 +2,3 @@ function openReviewPage(movieId) {
     // Redirige alla pagina del film
     window.location.href = `movie.html?id=${movieId}`;
 }
-
-
-
-document.getElementById("search-button").addEventListener("click", function() {
-    const query = document.getElementById("search-input").value.toLowerCase();
-    alert(`Searching for: ${query}`);
-    // Implementa la logica di ricerca qui
-});
---
+++ js/script_movieDetails.js
@@ -0,0 +1,27 @@
+
+
+// function renderRating(rating) {
+//     const ratingStars = document.getElementById("movie-rating-stars");
+//     const ratingValue = document.getElementById("movie-rating-value");
+
+//     const filledStars = Math.floor(rating);
+//     const halfStar = rating % 1 !== 0;
+//     const emptyStars = 5 - filledStars - (halfStar ? 1 : 0);
+
+//     let stars = "★".repeat(filledStars);
+//     if (halfStar) stars += "☆";
+//     stars += "☆".repeat(emptyStars);
+
+//     ratingStars.textContent = stars;
+//     ratingValue.textContent = `${rating.toFixed(1)}/5`;
+// }
+document.getElementById('movie-rating-value').addEventListener("load", renderRating);
+
+function renderRating() {
+    const starsFilled = document.getElementById("stars-filled");
+    const ratingValue = document.getElementById("movie-rating-value");
+
+    const percentage = (rating / 5) * 100;
+    starsFilled.style.width = `${percentage}%`;
+    ratingValue.textContent = `${rating.toFixed(1)}/5`;
+}
